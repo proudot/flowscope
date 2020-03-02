33d8751481065e25b7c5442de49d96fc127599e3
proudot <philippe.roudot@gmail.com>
2020-03-02 09:55:44 -0600

    Bug fix

--- sh-script/compOF.sh
+++ sh-script/compOF.sh
@@ -3,6 +3,6 @@ module load gcc/8.3.0
 module load tiff/gcc/4.0.7
 outputFolder=/archive/bioinformatics/Danuser_lab/softwareDevelopment/analysis/proudot/cytodi-OF3D/analysis/flowScopTest/
 mkdir $outputFolder
-#../flowscope/CSL2/build/CTvar -s ../mv3blebbing/1_CAM01_000004.tif -t ../mv3blebbing/1_CAM01_000008.tif -o $outputFolder
+../flowscope/CSL2/build/CTvar -s ../mv3blebbing/1_CAM01_000004.tif -t ../mv3blebbing/1_CAM01_000008.tif -o $outputFolder
 gmic $outputFolder/*soru.tif -a z -l[] $outputFolder/*sorv.tif -a z -endl -l[] $outputFolder/*soru.tif -a z -endl -a c \
      -normp 2 -o $outputFolder/mag.tif
